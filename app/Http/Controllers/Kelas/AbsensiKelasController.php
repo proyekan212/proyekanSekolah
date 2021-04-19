051a95dec5da8b88dc17619d784877ce08af9e78
verrandy <verrandyb@gmail.com>
2021-04-19 22:32:02 +0700

    fixing bugs absensi kelas

--- app/Http/Controllers/Kelas/AbsensiKelasController.php
+++ app/Http/Controllers/Kelas/AbsensiKelasController.php
@@ -11,14 +11,18 @@ class AbsensiKelasController extends BaseController
 {
     public function index(Request $request){
 
-        $daftarKelas = DaftarKelas::with(['kelas.jadwal_pelajaran.absen'])
+        $daftarKelas = DaftarKelas::with(['kelas.jadwal_pelajaran.absen', 'user_detail'])
         ->whereHas('kelas.jadwal_pelajaran', function($query) use($request) {
             $query->where('id', '=', $request->session()->get('kelas_mapel'));
-        })->first()
+        })
         ->where('kelas_id', $request->session()->get('kelas_id'))->get();
+        
+
+       
         return view('pages.kelas.absensikelas', [
             'daftar_kelas' => $daftarKelas
         ]);
+        
     }
 
     public function __construct()
