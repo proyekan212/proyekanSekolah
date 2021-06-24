b95849c87e708b16a9b115f3b5695976afeeee9a
proyekan212 <proyekan212@gmail.com>
2021-06-25 02:58:35 +0700

    Ubah Password User dan munculkan jadwal pelajaran sesuai tahun akademik aktif 2

--- resources/views/pages/profile/edit_password.blade.php
+++ resources/views/pages/profile/edit_password.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="modal-content">
   <div class="modal-header">
-    <h5 class="modal-title" id="exampleModalLabel">Penilaian Keterampilan</h5>
+    <h5 class="modal-title" id="exampleModalLabel">Ubah Password</h5>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
